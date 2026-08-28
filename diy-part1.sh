c0332bf2799765084312c803d29f6f72b9af1589
yfsuk <117582048+yfsuk@users.noreply.github.com>
2026-08-28 15:35:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,7 +2,15 @@
 
 sed -i '/^src-git helloworld /d' feeds.conf.default
 sed -i '/^src-git passwall /d' feeds.conf.default
+sed -i '/^src-git passwall_luci /d' feeds.conf.default
 sed -i '/^src-git passwall_packages /d' feeds.conf.default
 
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >> feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >> feeds.conf.default
+temp_file="$(mktemp)"
+
+{
+    echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main'
+    echo 'src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main'
+    cat feeds.conf.default
+} > "$temp_file"
+
+mv "$temp_file" feeds.conf.default
