21b97d4a8d626eb753491d2ddd1eb7f1f4e2942c
yfsuk <117582048+yfsuk@users.noreply.github.com>
2026-08-28 20:27:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,7 +8,6 @@ sed -i '/^src-git passwall_packages /d' feeds.conf.default
 temp_file="$(mktemp)"
 
 {
-    echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;25.8.26-1'
     echo 'src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;25.8.26-1'
     cat feeds.conf.default
 } > "$temp_file"
