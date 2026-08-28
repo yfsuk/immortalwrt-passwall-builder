d1d006899d16d837da0160200f45234c25c0c3e4
yfsuk <117582048+yfsuk@users.noreply.github.com>
2026-08-28 14:18:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 在首次启动时应用配置，执行成功后 OpenWrt 会自动删除此文件。
+mkdir -p files/etc/uci-defaults
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+cat > files/etc/uci-defaults/99-passwall-baseline <<'EOF'
+#!/bin/sh
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# Argon 作为 LuCI 默认主题。
+uci set luci.main.mediaurlbase='/luci-static/argon'
+uci commit luci
+
+# DHCP 由爱快提供，OpenWrt 不参与下发。
+uci set dhcp.lan.ignore='1'
+
+# 不在 OpenWrt LAN 下发 IPv6。
+uci set network.lan.ip6assign='0'
+uci set dhcp.lan.ra='disabled'
+uci set dhcp.lan.dhcpv6='disabled'
+uci set dhcp.lan.ndp='disabled'
+uci commit network
+uci commit dhcp
+
+# 单臂旁路由同接口 NAT 回程修复。
+grep -qxF 'net.bridge.bridge-nf-call-iptables=0' /etc/sysctl.conf || \
+    echo 'net.bridge.bridge-nf-call-iptables=0' >> /etc/sysctl.conf
+sysctl -w net.bridge.bridge-nf-call-iptables=0
+
+exit 0
+EOF
+
+chmod 0755 files/etc/uci-defaults/99-passwall-baseline
