845dda985a68446bb42878a45711dab1161f72f2
yfsuk <117582048+yfsuk@users.noreply.github.com>
2026-09-01 11:48:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
+set -eu
 
-sed -i '/^src-git helloworld /d' feeds.conf.default
-sed -i '/^src-git passwall /d' feeds.conf.default
-sed -i '/^src-git passwall_luci /d' feeds.conf.default
-sed -i '/^src-git passwall_packages /d' feeds.conf.default
+# Keep the image focused on HomeProxy. PassWall and HomeProxy must not manage
+# the same transparent-proxy nftables hooks in the initial compatibility test.
+sed -i '/^src-git \(helloworld\|passwall\|passwall_luci\|passwall_packages\|homeproxy\|mosdns\) /d' feeds.conf.default
 
 temp_file="$(mktemp)"
-
 {
-    echo 'src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;25.8.26-1'
+    echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy.git'
+    echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns.git'
     cat feeds.conf.default
 } > "$temp_file"
 
